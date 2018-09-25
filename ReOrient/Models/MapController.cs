e8b1fe6c57e875ed31db20d74077573f9d4089ff
MinYenYimYinYen <lukeholker@gmlawncare.onmicrosoft.com>
2018-09-25 16:29:03 -0500

    Created basic MapController & Record Classes

---
+++ ReOrient/Models/MapController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReOrient.Models
+{
+	public class MapController
+	{
+		public string MapCenter
+		{
+			get
+			{
+				return "20,20";
+			}
+		}
+		public string MapMode { get; set; }
+		public ObservableCollection<Record> Records { get; set; }
+	}
+}
---
+++ ReOrient/Models/Record.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReOrient.Models
+{
+	public class Record
+	{
+		public SA.markcust MarkCust { get; set; }
+
+	}
+}
