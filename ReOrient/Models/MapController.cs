dd199b2e437592c05b6c18525875cbd87fda3b33
MinYenYimYinYen <lukeholker@gmlawncare.onmicrosoft.com>
2018-09-25 16:52:21 -0500

    MapController: Added SizeLo, SizeHi & LoadRecoreds(). On Set SizeLo, SizeHi, ZipCode: LoadRecords() is called.

--- ReOrient/Models/MapController.cs
+++ ReOrient/Models/MapController.cs
@@ -17,6 +17,80 @@ namespace ReOrient.Models
 			}
 		}
 		public string MapMode { get; set; }
-		public ObservableCollection<Record> Records { get; set; }
+
+		private ObservableCollection<Record> records;
+		public ObservableCollection<Record> Records
+		{
+			get
+			{
+				if (records == null) records = new ObservableCollection<Record>();
+				return records;
+			}
+			set
+			{
+				records = value;
+			}
+		}
+
+		private decimal? sizeLo;
+		public decimal? SizeLo
+		{
+			get
+			{
+				return sizeLo;
+			}
+			set
+			{
+				sizeLo = value;
+				LoadRecords();
+			}
+		}
+
+		private decimal? sizeHi;
+		public decimal? SizeHi
+		{
+			get
+			{
+				return sizeHi;
+			}
+			set
+			{
+				sizeHi = value;
+				LoadRecords();
+			}
+		}
+
+		private string zipCode;
+		public string ZipCode
+		{
+			get
+			{
+				return zipCode;
+			}
+			set
+			{
+				zipCode = value;
+				LoadRecords();
+			}
+		}
+
+		private void LoadRecords()
+		{
+			if (SizeLo != null && SizeHi != null)
+			{
+				if (zipCode.Length == 5)
+				{
+					using (var sa = new SA.SAEntities1())
+					{
+						var recs = new ObservableCollection<Record>();
+						foreach (var mark in sa.markcusts.Where(m => m.zip.Trim().Substring(0, 5) == zipCode))
+						{
+							recs.Add(new Record { MarkCust = mark });
+						}
+						Records = recs;
+					}
+				}
+			}
+		}
 	}
 }
