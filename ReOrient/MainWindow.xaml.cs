04cb1eede6d154109aee03c9171b1a2c0209e8c7
Luke Holker - Work <LukeHolker@GMLawncare.onmicrosoft.com>
2018-09-26 14:00:01 -0500

    Kaibashed MapController.  Moved Contents to Mainwindow to gain access to dispatcher so I could run the query on another thread. Updated XAML accordingly.

--- ReOrient/MainWindow.xaml.cs
+++ ReOrient/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using ReOrient.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +26,111 @@ namespace ReOrient
 		public MainWindow()
 		{
 			InitializeComponent();
+
+		}
+		public string MapCenter
+		{
+			get
+			{
+				return "45.26653,-93.77274";
+			}
+		}
+		public string MapMode { get; set; }
+
+		private ObservableCollection<Record> records;
+		public ObservableCollection<Record> Records
+		{
+			get
+			{
+				if (records == null) records = new ObservableCollection<Record>();
+				return records;
+			}
+			set
+			{
+
+				records = value;
+			}
+		}
+
+		private decimal? sizeLo;
+		public decimal? SizeLo
+		{
+			get
+			{
+				return sizeLo;
+			}
+			set
+			{
+				sizeLo = value;
+				LoadRecordsOnThread();
+			}
+		}
+
+		private decimal? sizeHi;
+		public decimal? SizeHi
+		{
+			get
+			{
+				return sizeHi;
+			}
+			set
+			{
+				sizeHi = value;
+				LoadRecordsOnThread();
+			}
+		}
+
+		private string zipCode;
+		public string ZipCode
+		{
+			get
+			{
+				return zipCode;
+			}
+			set
+			{
+				zipCode = value;
+				LoadRecordsOnThread();
+			}
+		}
+		private void LoadRecordsOnThread()
+		{
+			Thread t = new Thread(new ThreadStart(this.LoadRecords));
+			t.Start();
+		}
+
+		private void LoadRecords()
+		{
+			if (SizeLo != null && SizeHi != null)
+			{
+				if (zipCode.Length == 5)
+				{
+					using (var sa = new SA.SADBContext())
+					{
+						var recs = new ObservableCollection<Record>();
+
+						//Filter By Zip
+						foreach (var mark in sa.markcusts
+							.Where(m => m.zip.Trim().Substring(0, 5) == zipCode))
+
+						{
+							recs.Add(new Record { MarkCust = mark });
+						}
+
+						//Filter by Size
+						var rMe = new ObservableCollection<Record>();
+						var filterSize = recs.Where(r => r.Size >= SizeLo)
+							.Where(r => r.Size <= SizeHi).ToList();
+
+						this.Dispatcher.Invoke(() => ClearRecords());
+
+						foreach (var mark in filterSize)
+						{
+							this.Dispatcher.Invoke(()=> Records.Add(new Record { MarkCust = mark.MarkCust }));							 
+						}
+					}
+				}
+			}
 		}
 	}
 }
