425a4807b7972d432cdf402a2d928d9ba5b8d63f
Luke Holker - Work <LukeHolker@GMLawncare.onmicrosoft.com>
2018-09-26 12:53:25 -0500

    Created Threading Folder Pulled in TextArgs.cs from LukesBrain

---
+++ ReOrient/Threading/TextArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReOrient.Threading
+{	
+	public class TextArgs : EventArgs
+	{
+		public TextArgs(string textMessage)
+		{
+			message = textMessage;
+		}
+
+		private string message;
+		public string Message
+		{
+			get
+			{
+				return message;
+			}
+			set
+			{
+				message = value;
+			}
+		}
+	}
+}
